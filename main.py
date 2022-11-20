433460986089cf21401b787585b43288a22b1b04
Barkhyunwoo <112528784+Barkhyunwoo@users.noreply.github.com>
2022-11-20 18:42:54 +0900

    베팅 시스템 추가 (#5)
    * 베팅 시스템 추가
    
    베팅시스템을 추가했습니다.
    
    * 블랙잭 게임 시스템 추가
    
    * 블랙잭 추가
    
    (21점일때 돈 2배 받는 시스템 추가)
    
    * 딜러 카드 받는 시스템 추가
    
    딜러 카드가 16점 이하일때 딜러는 카드를 받음

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def get_card():
         shape = "다이아몬드"
 
     return shape, n
+
 def calc_card():
     global cpu_s, you_s
 
@@ -45,16 +46,22 @@ def calc_card():
 def fight(player_result, dealer_result):  #플레이어와 컴퓨터의 카드값 비교
     if player_result == dealer_result:
         return 2
+    elif player_result > 21:
+        return 4
+    elif dealer_result > 21:
+        return 5
+    elif player_result == 21:
+        return 3
     elif player_result < dealer_result:
         return 0
     elif player_result > dealer_result:
         return 1
-    elif player_result == 21:
-        return 3
+    
+    
 
 def burst(player_result):  # 버스트인지 확인 (버스트 = 21이상)
     if (player_result > 21):
-        print("버스트입니다 게임종료")
+        return 1
 
 def get_fight_text(num):  #승패 판단
     if num == 0:
@@ -65,19 +72,78 @@ def get_fight_text(num):  #승패 판단
         return "Black Jack!"
     elif num == 2:
         return "Draw"
+    elif num == 4:
+        return "Bust!"
+    elif num == 5:
+        return "Dealer Bust! You Win!!"
 
-cpu = []
-you = []
+def race(): # 카드 더 받기 (hit일경우 더 받고 stay일 경우 멈춤)
+    dobak = input("카드를 더 받으실거면 hit, 멈추실거면 stay를 입력해주세요 : ") # 도박
+    if (dobak == 'hit'):
+        you.append(get_card())
+        return 0
+    elif (dobak == 'stay'):
+        return 1
+    else:
+        print("잘못입력하셨습니다.")
+        return 2
+        
 
-for j in range(2): # 블랙잭 기본 시작
-    cpu.append(get_card())
-    you.append(get_card())
+def beting(coin): # 베팅
+    bet = int(input("베팅할 금액을 입력하시오 : ")) # 베팅액 확인
+    if (bet > coin):
+        print("베팅한 금액이 가지고 있는 금액보다 큽니다.")
+        return beting(coin)
+    if bet <= 0:
+        print("양수를 입력해주세요")
+        return beting(coin)
+    return bet
+        
 
-calc_card()
+coin = 1000 # 초기 코인
 
-print("컴퓨터",cpu, cpu_s)
-print("플레이어",you, you_s)
+while True:
+    cpu = []
+    you = []
+    print("현재 가진 돈 :", coin)
+    if coin == 0:
+        print("더이상 가진 돈이 없습니다.")
+        break
+    bet = beting(coin) # 베팅액 확인
 
-burst(cpu_s)
+    for j in range(2): # 블랙잭 기본 시작
+        cpu.append(get_card())
+        you.append(get_card())
+        
+    calc_card()
 
-print(get_fight_text(fight(you_s, cpu_s)))
+    while True:
+        print("컴퓨터",cpu[0])
+        print("플레이어",you, you_s)
+        
+        if race() == 1: # 카드 더 받기
+            break
+        calc_card()
+        if burst(you_s) == 1: #버스트 판단
+            break
+        
+    while cpu_s < 17: #딜러는 16이하면 무조건 카드를 받아야함
+        cpu.append(get_card())
+        calc_card()
+    
+        
+    print("컴퓨터",cpu, cpu_s)
+    print("플레이어",you, you_s)
+
+    print(get_fight_text(fight(you_s, cpu_s)))
+
+    if fight(you_s, cpu_s) == 0 or fight(you_s, cpu_s) == 4:
+        coin = coin - bet
+    elif fight(you_s, cpu_s) == 1 or fight(you_s, cpu_s) == 5:
+        coin = coin + bet
+    elif fight(you_s, cpu_s) == 2:
+        coin = coin
+    elif fight(you_s, cpu_s) == 3:
+        coin = coin + bet*2
+       
+       
